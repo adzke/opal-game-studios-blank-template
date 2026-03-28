aaf41b53e14e4e5a3d3d3de57d9296cca3afbce7
Adrian Stockwell <adrian_stocky_@hotmail.com>
2026-03-28 12:17:42 +1000

    Run DB migrations on API startup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { buildApp } from './app.js';
-import { initDb } from './db.js';
+import { initDb, runMigrations } from './db.js';
 
 dotenv.config();
 
@@ -8,6 +8,7 @@ const port = Number(process.env.PORT || 3001);
 
 async function start() {
     await initDb();
+    await runMigrations();
     const { httpServer } = buildApp();
     httpServer.listen(port, '0.0.0.0', () => {
         console.log(`Server running on http://0.0.0.0:${port}`);
